9b7e91b6039ccbbf214840d602b7cf1a9e37f7c2
Ismael Darias <alu0101576602@ull.edu.es>
2026-03-30 10:57:02 +0100

    Corrección: Había una línea que duplicaba archivos. Ha sido corregida

--- ReadOriginalData.py
+++ ReadOriginalData.py
@@ -202,7 +202,7 @@ def correct_sections(
                         years_ = ""
                         for year in years:
                             years_ += year + "_"
-                            ds.to_netcdf(dst_path + section + "/" + years_ + f)
+                        ds.to_netcdf(dst_path + section + "/" + years_ + f)
                 
                 else:    
                     ds = xr.open_dataset(f_path)
